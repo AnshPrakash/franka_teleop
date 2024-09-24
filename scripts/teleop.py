eb01ab24cff304a11e52d459c12db4a97cb600a4
Alina <alinab1809@gmail.com>
2024-09-24 15:53:48 +0000

    Get controllers running in simulation

--- scripts/teleop.py
+++ scripts/teleop.py
@@ -1,9 +1,19 @@
 import rospy
 from geometry_msgs.msg import Pose, PoseStamped, PointStamped
-from controller_manager_msgs.srv import ListControllers
+from controller_manager_msgs.srv import SwitchController, LoadController, UnloadController, ReloadControllerLibraries, ListControllers
+from actionlib import SimpleActionClient
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal, FollowJointTrajectoryResult
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from std_srvs.srv import Trigger, TriggerResponse
+
+import copy
 import numpy as np
 import tf
 import time
+from tf.transformations import quaternion_matrix, quaternion_from_matrix, rotation_matrix, concatenate_matrices
+
+from util import go_to, attempt_to_go_to_joints
 
 class Teleop():
 
@@ -18,6 +28,8 @@ class Teleop():
         self.ee_quat = None
 
         self.gripper_open = None
+
+        self.rate = rospy.Rate(50)
         ############## Subscribers ######################
 
         # VIVE controller pose
@@ -33,10 +45,19 @@ class Teleop():
         ############## TF ###############################
         self.tf_listener = tf.TransformListener()
 
-    ################################# Controller Pose #####################################################
+    ################################# ROS callback funnctions #####################################################
     def vive_pose_cb(self, msg):
+        # Saves vive controller pose to gloabl variable
         self.vive_last_right_pose = msg
 
+    def __process_ee_pose(self, data):
+        # Callback to get EE pose
+        self.ee_pos = np.asarray([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+        self.ee_quat = np.asarray([data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w])
+        print("EE pose:",self.ee_pos)
+
+
+    ########################################## Util functions #######################################################
     def get_NE_pose(self): 
         # Function to get EE pose from tf
         while True:
@@ -47,13 +68,6 @@ class Teleop():
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 continue
         return trans, rot
-
-
-    def __process_ee_pose(self, data):
-        # Callback to get EE pose
-        self.ee_pos = np.asarray([data.pose.position.x, data.pose.position.y, data.pose.position.z])
-        self.ee_quat = np.asarray([data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w])
-        print("EE pose:",self.ee_pos)
 
     def get_pose_info(self, pose):
         # Gets information from Pose msg into lists
@@ -68,7 +82,7 @@ class Teleop():
 
         return [x,y,z], [rot_x,rot_y,rot_z,rot_w]
 
-    
+    ############################################### Desired pose/state functions ############################################
     def publish_eef_target(self, pos, quat):
         msg = PoseStamped()
         msg.header.stamp = rospy.Time.now()
@@ -94,6 +108,7 @@ class Teleop():
 
         self.pub_gripper.publish(msg_gripper)
 
+    ############################################# ROS controllers functions #############################################
 
     def list_controllers(self):
         rospy.wait_for_service("/controller_manager/list_controllers")
@@ -115,11 +130,10 @@ class Teleop():
         switch_controller = rospy.ServiceProxy("/controller_manager/switch_controller", SwitchController)
         switch_controller(start_controllers, stop_controllers, 0, False, 0.0)
 
+
+    ######################################### Gripper functions ##################################################
     def open_gripper(self):
         self.gripper_open = True
-
-    def close_gripper(self):
-        self.gripper_open = False
 
     def open_gripper_w_msg(self):
         # function is needed as otherwise the desired gripper state is only sent upon the node being active!
@@ -141,6 +155,87 @@ class Teleop():
         time.sleep(1)
 
 
+    def close_gripper(self):
+        self.gripper_open = False
+
+    ############################################ Robot motion scripts ###############################################
+    def startup_procedure(self, desired_joint_config=None, initial_config_pose=None):
+
+        self.cartesian_pose_impedance_controller_loaded = False
+        self.cartesian_pose_impedance_controller_running = False
+
+        self.effort_joint_trajectory_controller_loaded = False
+        self.effort_joint_trajectory_controller_running = False
+
+        list_controller_res = self.list_controllers()
+        for i in range(len(list_controller_res.controller)):
+            if (list_controller_res.controller[i].name == "cartesian_pose_impedance_controller"):
+                self.cartesian_pose_impedance_controller_loaded = True
+                if (list_controller_res.controller[i].state == "running"):
+                    self.cartesian_pose_impedance_controller_running = True
+
+            if (list_controller_res.controller[i].name == "effort_joint_trajectory_controller"):
+                self.effort_joint_trajectory_controller_loaded = True
+                if (list_controller_res.controller[i].state == "running"):
+                    self.effort_joint_trajectory_controller_running = True
+
+
+        action = rospy.resolve_name('effort_joint_trajectory_controller/follow_joint_trajectory')
+        client = SimpleActionClient(action, FollowJointTrajectoryAction)
+        rospy.loginfo("move_to_start: Waiting for '" + action + "' action to come up")
+        client.wait_for_server()
+
+        topic = rospy.resolve_name('franka_state_controller/joint_states')
+        rospy.loginfo("move_to_start: Waiting for message on topic '" + topic + "'")
+        joint_state = rospy.wait_for_message(topic, JointState)
+        initial_pose = dict(zip(joint_state.name, joint_state.position))
+
+        # open gripper first
+        self.open_gripper_w_msg()
+
+        if not(self.cartesian_pose_impedance_controller_loaded and self.effort_joint_trajectory_controller_running):
+            time.sleep(0.5)
+            # if we do not start up for the first time - we first need to switch back!
+            self.switch_controller(["effort_joint_trajectory_controller"], ["cartesian_pose_impedance_controller"])
+
+        if (desired_joint_config is None):
+            desired_joint_config = np.array([0.004286136549292948, 0.23023615878924988, -0.003981800034836296, -1.7545947008261213,
+                                             0.0032928755527341326, 1.994446315732633, 0.7839058620188021])
+
+        attempt_to_go_to_joints(client, topic, desired_joint_config, duration=5)
+
+        # before switching, save current eef_pos and eef_quat
+        curr_eef_pos = copy.deepcopy(self.ee_pos)
+        curr_eef_quat = copy.deepcopy(self.ee_quat)
+
+
+        # after moving to the desired pose, switch the controller to the effort joint trajectory!
+        if not(self.cartesian_pose_impedance_controller_loaded):
+            self.load_controller("cartesian_pose_impedance_controller")
+            time.sleep(0.5)
+
+        self.switch_controller(["cartesian_pose_impedance_controller"], ["effort_joint_trajectory_controller"])
+
+
+
+    def move_robot(self):
+        trans_EE, rot_EE = self.get_NE_pose()
+
+        msg = PoseStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = 'panda_link0'
+        # add some scaling such that the robot can be moved more easily
+        msg.pose.position.x = trans_EE[0] +0.2
+        msg.pose.position.y = trans_EE[1] +0.2
+        msg.pose.position.z = trans_EE[2] +0.1
+
+        msg.pose.orientation.x = rot_EE[0]
+        msg.pose.orientation.y = rot_EE[1]
+        msg.pose.orientation.z = rot_EE[2]
+        msg.pose.orientation.w = rot_EE[3]
+
+        self.pub.publish(msg)
+
     def run(self):
         while not rospy.is_shutdown():
 
@@ -152,13 +247,15 @@ class Teleop():
             # Get VIVE controller pose
             vive_position, vive_orientation = self.get_pose_info(self.vive_last_right_pose)
             print("VIVE position:", vive_position)
+            
 
 
 
 
 if __name__ == '__main__':
     teleop = Teleop()
-    teleop.run()
+    teleop.startup_procedure()
+    teleop.move_robot()
     
 
 
---
+++ scripts/util.py
@@ -0,0 +1,60 @@
+import copy
+import rospy
+import actionlib
+import numpy as np
+from threading import Lock
+from typing import List, Union
+from sensor_msgs.msg import JointState
+from control_msgs.msg import FollowJointTrajectoryGoal, FollowJointTrajectoryAction, FollowJointTrajectoryResult
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from typing import Optional
+
+
+def go_to(trajectory_client: actionlib.SimpleActionClient, joint_names: List[str],
+          init_positions: Union[np.ndarray, List[float]], target_positions: Union[np.ndarray, List[float]],
+          duration: float = None, max_vel: float = None, wait: bool = True, time_offset: float = 0.0):
+    if duration is None:
+        if max_vel is None:
+            raise RuntimeError("If duration is not specified, the maximum allowed joint velocity must be specified")
+
+        duration = max(0.5, np.linalg.norm(np.array(init_positions) - np.array(target_positions)) / max_vel)
+
+    # Create a trajectory in which we simply move J0 by 20 degrees
+    traj = JointTrajectory()
+    traj.header.stamp = rospy.Time.now() + rospy.Duration.from_sec(time_offset)
+    traj.joint_names = joint_names
+
+    traj_point = JointTrajectoryPoint()
+    traj_point.time_from_start = rospy.Duration(duration)
+    traj_point.positions = target_positions
+    traj_point.velocities = [0] * len(target_positions)
+    traj.points = [traj_point]
+
+    traj_goal = FollowJointTrajectoryGoal()
+    traj_goal.trajectory = traj
+    if wait:
+        trajectory_client.send_goal_and_wait(traj_goal)
+        return trajectory_client.get_result()
+    else:
+        trajectory_client.send_goal(traj_goal)
+
+
+def attempt_to_go_to_joints(client, topic, desired_config, duration=5):
+    '''
+    This function implements the behavior to go to a certain desired joint configuration
+    '''
+    # get current joint state
+    joint_state = rospy.wait_for_message(topic, JointState)
+    # try at maximum 5 times
+    for i in range(5):
+        # then go to default pose
+        result = go_to(client, joint_state.name[:7], joint_state.position[:7], desired_config, duration=duration)
+        print(result)
+        if (result.error_code == FollowJointTrajectoryResult.SUCCESSFUL):
+            print("The robot was successfully moved and initialized")
+            break
+        else:
+            # roserror message
+            rospy.logerr("The robot was not able to move and initialized")
+            time.sleep(1)
+
