6f6a330cfaa7b138686df7b4a2fb06b304852272
Ansh Prakash <anshprakash1997@gmail.com>
2026-01-11 20:37:09 +0100

    fix setup.py to detect launch and config files

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 import os
+import glob
+
+package_name = 'franka_teleop'
+
+
 
 if __name__ == '__main__':
     # compute absolute file URLs to subpackages
@@ -9,6 +14,7 @@ if __name__ == '__main__':
 
     # PEP 508 direct references (pip will install from these paths)
     install_requires = [
+        'setuptools',
         'numpy',
         'hydra-core',
         'omegaconf',
@@ -16,7 +22,35 @@ if __name__ == '__main__':
         f"oculus_reader @ file://{oculus_path}",
     ]
 
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, glob.glob('launch/**')),
+        ('share/' + package_name, glob.glob('config/**')),
+    ]
+
+
     setup(
-        setup_requires=['setuptools>=40.8.0'],
+        name=package_name,
+        version='0.0.1',
+        packages=[package_name],
+        data_files=data_files,
         install_requires=install_requires,
+        zip_safe=True,
+        author='Ansh Prakash',
+        author_email='todo@email.org',
+        keywords=['ROS1'],
+        classifiers=[
+            'Intended Audience :: Developers',
+            'Programming Language :: Python',
+            'Topic :: Software Development',
+        ],
+        description='Franka Emika Panda Teleoperation using Oculus VR Controller',
+        license='Apache License, Version 2.0',
+        entry_points={
+            'console_scripts': [
+                'oculus_transforms_vrpolicy = franka_teleop.oculus_transforms_vrpolicy:main',
+                'teleop_quest = franka_teleop.teleop_quest:main',
+            ],
+        },
     )
